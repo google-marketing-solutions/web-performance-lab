a3f5c031eb7b30982e2db8592bbe126c2dce82de
Markus Bordihn <mbordihn@google.com>
2023-06-27 16:22:50 +0200

    Updated documentation and add team names csv import for results_tool. (#16)
    Added crux tablet report example.
    Added functionality to add team names directly into the .csv file.
    Added build rules to allow easier developer mode and production mode.
    Added additional example file for teams.
    Added bundle.js to repro for GitHub pages support.
    Optimized images and overall bundle size.

--- speed_games/results_tool/scripts/libraries/countdown.js
+++ speed_games/results_tool/scripts/libraries/countdown.js
@@ -7,42 +7,42 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 export default () => {
-    document.querySelector('#counter-scene').style.display = 'block';
-    document.querySelector('#scene').style.display = 'none'
-    document.querySelector('#input-scene').style.display = 'none'
-    document.querySelector('#waiting-scene').style.display = 'none'
+  document.querySelector('#counter-scene').style.display = 'block';
+  document.querySelector('#scene').style.display = 'none';
+  document.querySelector('#input-scene').style.display = 'none';
+  document.querySelector('#waiting-scene').style.display = 'none';
 
-    const nums = document.querySelectorAll('.nums span');
-    const counter = document.querySelector('.counter');
-    const finalMessage = document.querySelector('.final');
+  const nums = document.querySelectorAll('.nums span');
+  const counter = document.querySelector('.counter');
+  const finalMessage = document.querySelector('.final');
 
-    function resetDOM() {
-        counter.classList.remove('hide');
-        finalMessage.classList.remove('show');
+  function resetDOM() {
+    counter.classList.remove('hide');
+    finalMessage.classList.remove('show');
 
-        nums.forEach(num => {
-            num.classList.value = '';
-        });
-        nums[0].classList.add('in');
-    }
+    nums.forEach((num) => {
+      num.classList.value = '';
+    });
+    nums[0].classList.add('in');
+  }
 
-    resetDOM();
-    runAnimation();
+  resetDOM();
+  runAnimation();
 
-    function runAnimation() {
-        nums.forEach((num, idx) => {
-            const penultimate = nums.length - 1;
-            num.addEventListener('animationend', (e) => {
-                if (e.animationName === 'goIn' && idx !== penultimate) {
-                    num.classList.remove('in');
-                    num.classList.add('out');
-                } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-                    num.nextElementSibling.classList.add('in');
-                } else {
-                    counter.classList.add('hide');
-                    finalMessage.classList.add('show');
-                }
-            });
-        });
-    }
-}
+  function runAnimation() {
+    nums.forEach((num, idx) => {
+      const penultimate = nums.length - 1;
+      num.addEventListener('animationend', (e) => {
+        if (e.animationName === 'goIn' && idx !== penultimate) {
+          num.classList.remove('in');
+          num.classList.add('out');
+        } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+          num.nextElementSibling.classList.add('in');
+        } else {
+          counter.classList.add('hide');
+          finalMessage.classList.add('show');
+        }
+      });
+    });
+  }
+};
